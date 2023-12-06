84bd0bac6684ff19ea884e612ee3e5cb41f1dfd8
Julievogh <juvo0001@stud.kea.dk>
2023-12-06 11:28:32 +0100

    navbar

---
+++ src/scripts/generel.js
@@ -0,0 +1,6 @@
+document.getElementById("mobile-menu").addEventListener("click", function () {
+  var menu = document.getElementById("menu");
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+});
+
+console.log("hello");
